040bdab39d0ccdd7b7b0cc2c03225640fbf906d6
petchi muthu <petchimuthub2007@gamil.com>
2026-07-11 22:00:21 +0530

    Update browser camera and Railway deployment

--- database.py
+++ database.py
@@ -1,13 +1,18 @@
-import mysql.connector
 import os
+import mysql.connector
 
 def get_connection():
-    connection = mysql.connector.connect(
+    print("DB_HOST =", os.getenv("DB_HOST"))
+    print("DB_PORT =", os.getenv("DB_PORT"))
+    print("DB_NAME =", os.getenv("DB_NAME"))
+    print("DB_USER =", os.getenv("DB_USER"))
+
+    return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        database=os.getenv("DB_NAME")
     )
     return connection
 
